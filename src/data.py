9d573ca9ca495af5a862b6c6a76bc668a78645a7
Dhravya <dhravyashah@gmail.com>
2022-07-13 16:22:04 +0530

    Updated rules

--- src/data.py
+++ src/data.py
@@ -1,7 +1,7 @@
 RULES = {
     "add": [
         {
-            "value": """(meme OR memes) has:images -is:retweet lang:en -is:reply -contest -instagram -blacksheep -anime -crypto -nft -coins -politics""",
+            "value": """(meme OR memes) has:images -is:retweet lang:en -is:reply -contest -instagram -blacksheep -anime -crypto -nft -coins -politics -comic -manhwa""",
             "tag": "new_memes",
         },
     ]
