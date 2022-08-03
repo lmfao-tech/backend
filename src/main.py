cd9e0c9745e19972730c552f632c1e303f7c6b53
Dhravya <dhravyashah@gmail.com>
2022-08-03 16:56:52 +0530

    refactored code and added upload endpoint

--- src/main.py
+++ src/main.py
@@ -3,18 +3,18 @@ from typing import Optional
 from os import environ as env
 
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
 from rich import print
+from pytwitter import Api
 from dotenv import load_dotenv
 from pytwitter import StreamApi
-from pytwitter import Api
 from rich.traceback import install
+from fastapi import FastAPI, Request
 from datetime import datetime, timedelta
+from fastapi.responses import RedirectResponse
 
-from helpers import reset_rules, shuffle_list, repeat_every
 from _types import Tweet
 from server import Server
+from helpers import reset_rules, shuffle_list, repeat_every
 
 load_dotenv()
 
@@ -47,24 +47,30 @@ def filter_tweet(tweet: Tweet) -> Optional[Meme]:
     )
 
     if created_at > datetime.now() - timedelta(days=10):
-        print("[red]User acc created less than 10 days ago, skipping[/red]")
+        if dev:
+            print("[red]User acc created less than 10 days ago, skipping[/red]")
         return
     if not "media" in tweet["includes"]:
-        print("[red]No media, skipping[/red]")
+        if dev:
+            print("[red]No media, skipping[/red]")
         return
     if not tweet["includes"]["media"][0]["type"] == "photo":
-        print("[red]Not a photo, skipping[/red]")
+        if dev:
+            print("[red]Not a photo, skipping[/red]")
         return
     if tweet["data"]["text"].startswith("RT "):
-        print("[red]Is a retweet, skipping[/red]")
+        if dev:
+            print("[red]Is a retweet, skipping[/red]")
         return
 
     if any(keyword in tweet["data"]["text"].lower() for keyword in blocked.keywords):
-        print("[red]Is an ad, skipping[/red]")
+        if dev:
+            print("[red]Is an ad, skipping[/red]")
         return
 
     if tweet["includes"]["users"][0]["username"].lower() in " ".join(blocked.users):  # type: ignore
-        print("[red]Is a blocked user, skipping[/red]")
+        if dev:
+            print("[red]Is a blocked user, skipping[/red]")
         return
 
     if "urls" in tweet["data"]["entities"]:
@@ -72,7 +78,8 @@ def filter_tweet(tweet: Tweet) -> Optional[Meme]:
             url in tweet["data"]["entities"]["urls"][0]["expanded_url"]
             for url in blocked.urls  # type: ignore
         ):
-            print("[red]Is a blocked url, skipping[/red]")
+            if dev:
+                print("[red]Is a blocked url, skipping[/red]")
             return
 
     meme = Meme(
@@ -266,61 +273,80 @@ async def community_memes(last_id: int = 0, max_tweets: int = 20):
 
 # * MODERATION ENDPOINTS
 # TODO: Redis cache for the moderation endpoints
-# @app.get("/revive_meme")
-# async def revive_post(id: str):
+@app.get("/revive_meme")
+async def revive_post(id: str):
 
-#     global new_memes, top_memes_, community_memes_, removed_memes_list
+    for i, d in enumerate(cache.removed_memes):
+        if d.tweet_id == id:
+            cache.removed_memes.pop(i)
+            cache.memes.insert(0, d)
 
-#     for i, d in enumerate(removed_memes_list):
-#         if d["tweet_id"] == id:
-#             removed_memes_list.pop(i)
-#             new_memes.append(d)
-
-#     return {"message": "done"}
+    return {"message": "done"}
 
 
-# @app.get("/removed_memes")
-# async def removed_memes(last: int = 0, max_tweets: int = 20):
-#     """Get the removed memes stored in cache"""
-#     global removed_memes_list
+@app.get("/removed_memes")
+async def removed_memes(last_id: int = 0, max_tweets: int = 20):
+    if last_id == 0:
+        return cache.removed_memes[:max_tweets]
 
-#     return removed_memes_list[last : last + max_tweets]
+    # Get the meme with the last_id
+    for meme in cache.removed_memes:
+        if meme.tweet_id == str(last_id):
+            return cache.removed_memes[
+                cache.removed_memes.index(meme)
+                + 1 : cache.removed_memes.index(meme)
+                + 1
+                + max_tweets
+            ]
+
+    return cache.removed_memes[:max_tweets]
 
 
-# @app.get("/remove_meme")
-# async def remove_a_post(id: str, by: str):
+@app.get("/remove_meme")
+async def remove_a_post(id: str, by: str):
 
-#     global new_memes, top_memes_, community_memes_, removed_memes_list
-#     da_meme = None
+    da_meme = None
 
-#     for i, d in enumerate(new_memes):
-#         if d["tweet_id"] == id:
-#             da_meme = d
-#             new_memes.pop(i)
-#     for i, d in enumerate(top_memes_):
-#         if d["tweet_id"] == id:
-#             da_meme = d
-#             top_memes.pop(i)
-#     for i, d in enumerate(community_memes_):
-#         if d["tweet_id"] == id:
-#             da_meme = d
-#             community_memes_.pop(i)
+    for i, d in enumerate(cache.memes):
+        if d.tweet_id == id:
+            da_meme = d
+            cache.memes.pop(i)
+    for i, d in enumerate(cache.top_memes):
+        if d.tweet_id == id:
+            da_meme = d
+            cache.top_memes.pop(i)
+    for i, d in enumerate(cache.community_memes):
+        if d.tweet_id == id:
+            da_meme = d
+            cache.community_memes.pop(i)
 
-#     already_exists = False
-#     for e in removed_memes_list:
-#         if e["tweet_id"] == id:
-#             already_exists = True
+    already_exists = False
+    for e in cache.removed_memes:
+        if e.tweet_id == id:
+            already_exists = True
 
-#     if not already_exists and da_meme:
-#         da_meme["removed_by"] = by
-#         removed_memes_list.append(da_meme)
+    if not already_exists and da_meme:
+        da_meme.removed_by = by
+        da_meme.expire(num_seconds=60 * 60 * 12)
+        cache.removed_memes.append(da_meme)
 
-#     return {"message": "done"}
+    return {"message": "done"}
+
+
+# * UPLOAD ENDPOINTS
+@app.post("/upload_meme")
+async def upload_meme(data: Meme):
+    """Upload a meme to the server"""
+    print(f"[blue]Uploading meme: {data.tweet_text}[/blue]")
+    cache.community_memes.insert(0, data)
+    cache.save()
+    return {"message": "done"}
+
 
 config = uvicorn.Config(app=app, host="0.0.0.0")
 if dev:
     print("[green]Running in development mode[/green]")
-    config = uvicorn.Config(app="main:app", reload=True, debug=True)
+    config = uvicorn.Config(app=app, reload=True, debug=True)
 server = Server(config)
 
 
